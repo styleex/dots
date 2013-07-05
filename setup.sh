a8990f661f302c7c65a594d310731c7bde7577ae
StyleEX <styleex@xakep.ru>
2013-07-05 18:16:31 +0600

    Git config

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-BASEDIR=$(dirname $0)
-ln -s $BASEDIR/dots/mysql/.my.cnf ~/.my.cnf
-ln -s $BASEDIR/dots/grc/.grcat ~/.grcat
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname $SCRIPT)
+
+ln -s $SCRIPTPATH/dots/mysql/.my.cnf ~/.my.cnf
+ln -s $SCRIPTPATH/dots/grc/.grcat ~/.grcat
+ln -s $SCRIPTPATH/dots/git/.gitconfig ~/.gitconfig
