5469ef9262d3e5074f28c26a9eb2047bc4adaff9
StyleEX <styleex@xakep.ru>
2013-07-06 18:28:01 +0600

    Bash setup

--- setup.sh
+++ setup.sh
@@ -4,8 +4,12 @@ SCRIPT=$(readlink -f $0)
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname $SCRIPT)
 
+rm ~/.bashrc
+
 ln -s $SCRIPTPATH/dots/mysql/.my.cnf ~/.my.cnf
 ln -s $SCRIPTPATH/dots/grc/.grcat ~/.grcat
 ln -s $SCRIPTPATH/dots/git/.gitconfig ~/.gitconfig
 ln -s $SCRIPTPATH/dots/vim/.vim ~/
 ln -s $SCRIPTPATH/dots/vim/.vimrc ~/.vimrc
+ln -s $SCRIPTPATH/dots/bash/.bashrc ~/.bashrc
+ln -s $SCRIPTPATH/dots/tmux/.tmux.conf ~/.tmux.conf
