4de29fcaa5c7e090cc71028d052a409aedd9af73
Антон Владимиров <styleex@inbox.ru>
2015-04-04 23:30:13 +0500

    Fix

--- dots/go/install.sh
+++ dots/go/install.sh
@@ -1,4 +1,9 @@
 git clone https://github.com/canha/golang-tools-install-script.git /tmp/dots_goinstaller
+chmod +x /tmp/dots_goinstaller/main.sh
 /tmp/dots_goinstaller/main.sh --remove
 /tmp/dots_goinstaller/main.sh --64
 rm -rf /tmp/dots_goinstaller
+
+# FIX main.sh
+mkdir -p "$HOME"/go/{src,pkg,bin}
+rm -rf "$HOME/go/{sr*"
