f4a6a199238d68d2862d74a2e9fca87a3930a330
Антон <styleex@users.noreply.github.com>
2022-10-04 13:06:03 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,18 +4,18 @@ SCRIPT=$(readlink -f $0)
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname $SCRIPT)
 
-rm ~/.bashrc
+#rm ~/.bashrc
 rm ~/.vimrc -f
 rm ~/.vim -Rf
 
 sudo apt-get update && sudo apt-get install vim-nox
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
-ln -s $SCRIPTPATH/dots/mysql/.my.cnf ~/.my.cnf
-ln -s $SCRIPTPATH/dots/grc/.grcat ~/.grcat
-ln -s $SCRIPTPATH/dots/git/.gitconfig ~/.gitconfig
+#ln -s $SCRIPTPATH/dots/mysql/.my.cnf ~/.my.cnf
+#ln -s $SCRIPTPATH/dots/grc/.grcat ~/.grcat
+#ln -s $SCRIPTPATH/dots/git/.gitconfig ~/.gitconfig
 ln -s $SCRIPTPATH/dots/vim/.vimrc ~/.vimrc
-ln -s $SCRIPTPATH/dots/bash/.bashrc ~/.bashrc
+#ln -s $SCRIPTPATH/dots/bash/.bashrc ~/.bashrc
 ln -s $SCRIPTPATH/dots/tmux/.tmux.conf ~/.tmux.conf
 
 vim +PluginInstall +qall
